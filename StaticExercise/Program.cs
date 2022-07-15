718cbf25cfc23fc75a76a393a2af0ea8eb118de2
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-15 15:16:08 -0400

    Created a static class and called its methods

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"42 degress fahrenheit is equal to {TempConverter.FahrenheitToCelsius(42.0)} degrees celsius");
+            Console.WriteLine($"42 degress celsius is equal to {TempConverter.CelsiusToFahrenheit(42.0)} degrees fahrenheit");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		static TempConverter()
+		{
+		}
+
+		public static double FahrenheitToCelsius(double fTemp)
+        {			
+			double cTemp = (fTemp - 32) * (5.0/9.0);
+
+			return cTemp;
+        }
+
+		public static double CelsiusToFahrenheit(double cTemp)
+        {
+			double fTemp = (cTemp * (9.0 / 5.0)) + 32.0;
+
+			return fTemp;
+        }
+	}
+}
